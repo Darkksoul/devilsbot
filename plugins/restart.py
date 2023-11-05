c72d30edc5ea2405a9eaa9c53d0fe4a0a0380349
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2023-11-05 15:33:59 +0530

    Create restart.py

---
+++ plugins/restart.py
@@ -0,0 +1,13 @@
+import re, asyncio, os, sys
+from pyrogram import Client, filters, enums
+from pyrogram.types import *
+from config import OWNER
+from bot import Client
+
+
+@Client.on_message(filters.private & filters.command(["restart", "r"]) & filters.user(OWNER) ) 
+async def stop_button(bot, message):
+    msg = await bot.send_message(text="<i>Restarting.....</i>", chat_id=message.chat.id)       
+    await asyncio.sleep(10)
+    await msg.edit("<i>๏[-ิ_•ิ]๏ bot restarted !</i>")
+    os.execl(sys.executable, sys.executable, *sys.argv)
